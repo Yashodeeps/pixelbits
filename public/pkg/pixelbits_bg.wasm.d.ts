e0d020d11fa8bf05b1e03b787a2aa9d7f395692a
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2025-06-07 13:37:32 +0530

    Add WebAssembly type definitions for pixelbits_bg.wasm

---
+++ public/pkg/pixelbits_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const mangafy: (a: number, b: number, c: number, d: number, e: any) => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
